6626c3ba2a9eab43bd91dfef7cdecc4186524605
Erics-Lab <61784292+Erics-Lab@users.noreply.github.com>
2020-10-27 02:33:48 +0800

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,7 @@
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+
+wavesurfer.load('../asset/blackbear - sobbing in cabo');
